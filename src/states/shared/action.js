1e034ee71d35165621a40249de26b492e95f2056
Rohman <rohmanpendo@gmail.com>
2022-11-20 16:07:38 +0700

    feat: shared action state

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,17 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { receiveTalkActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+export function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveTalkActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
