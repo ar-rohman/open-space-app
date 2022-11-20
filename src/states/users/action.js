7da89495b168dfcc5dd613ef75e024639d835775
Rohman <rohmanpendo@gmail.com>
2022-11-20 15:25:55 +0700

    feat: user state

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,22 @@
-/**
- * @TODO: Define all the actions (creator) for the users state
- */
+import api from '../../utils/api';
+
+export const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+export function receiveUsersActionCreator(users) {
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: { users },
+  };
+}
+
+export function asyncRegisterUser({ id, name, password }) {
+  return async () => {
+    try {
+      await api.register({ id, name, password });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
