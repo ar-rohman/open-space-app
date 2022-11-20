d5453043a89665a80e0b27446fac61619c89a091
Rohman <rohmanpendo@gmail.com>
2022-11-20 15:16:45 +0700

    feat: isPerload state

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,26 @@
-/**
- * @TODO: Define all the actions (creator) for the isPreLoad state
- */
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+export const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+export function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: { isPreload },
+  };
+}
+
+export function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
--- src/states/isPreload/reducer.js
+++ src/states/isPreload/reducer.js
@@ -1,3 +1,10 @@
-/**
- * @TODO: Define reducer for the isPreLoad state
- */
+import { ActionType } from './action';
+
+export default function isPerloadReducer(isPreload = true, action = {}) {
+  switch (action.type) {
+    case ActionType.SET_IS_PRELOAD:
+      return action.payload.isPreload;
+    default:
+      return isPreload;
+  }
+}
